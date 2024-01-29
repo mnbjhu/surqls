40a026a9c76f6d40e9e47414425b6af4fd520d62
James Gibson <james-gibson@ntlworld.com>
2024-01-29 18:24:07 +0000

    added extension

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -X POST \
-	-u "root:root" \
-	-H "NS: test" \
-	-H "DB: test" \
-	-H "Accept: application/json" \
-	-d "create test content { data: { first: \"first\", second: \"second\", } };" \
-	http://localhost:8000/sql
